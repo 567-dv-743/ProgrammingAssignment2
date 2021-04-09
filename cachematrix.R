892d8ab2193da51febdb1974204ce982810326f6
567-dv-743 <74860952+567-dv-743@users.noreply.github.com>
2021-04-09 13:48:34 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+rm(list = ls())
 
-## Write a short comment describing this function
+## The script contains two functions: makeCacheMatrix and cacheSolve.
+## 1) makeCacheMatrix takes as input a matrix and creates getters and setters for 
+## both the matrix and its inverse.
+## 2) cacheSolve takes as input a makeCacheMatrix object, and either gets and 
+## returns the previously calculated matrix inverse OR, if there is no previously
+## calculated inverse, calculates it, stores it in the makeCacheMatrix object, and 
+## then returns it.
+
+## makeCacheMatrix takes as input a matrix and creates getters and setters for 
+## both the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function(){
+        x
+    } 
+    
+    setinverse <- function(inverse){
+        inv <<- inverse
+    }
+    
+    getinverse <- function(){
+        inv
+    }
+    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as input a makeCacheMatrix object, and either gets and returns
+## the previously calculated matrix inverse OR, if there is no previously
+## calculated inverse, calculates it, stores it in the makeCacheMatrix object, and 
+## then returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinverse()
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
